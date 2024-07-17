dceeeed580867539e6424b9941dafdf57573d106
r-e-g-ular <110486812+r-e-g-ular@users.noreply.github.com>
2024-07-17 17:07:39 +0000

    Cluster Scripts for Setting Modules and Virtual Env
    5 of these scripts (Activate* and *Job.sh) follow the same template for loading modules, checking that they are loaded, and then proceeding to run a job.
    
    The InteractiveJob.sh is simply to quickly request an interactive session with X11 forwarding (instructions for MacOS to be added to local set up).
    
    All files are WIP.

--- samples/slurm/activateASEVasp.sh
+++ samples/slurm/activateASEVasp.sh
@@ -28,3 +28,34 @@ else
 	echo "Exiting"
 	exit 1
 fi
+
+#!/bin/bash
+
+module purge
+module load StdEnv/2023 intel/2023.2.1 openmpi/4.1.5
+module load vasp/6.4.2
+module load python/3.11.5 scipy-stack
+
+if [[ $(module list | grep 'intel/2023.2.1') == ""  || $(module list | grep 'python/3.11.5') == "" || $(module list | grep 'vasp/6.4.2') == "" ]]; then
+	echo "Your modules are not loaded correctly. Cancelling job... "
+	exit 1
+else
+	echo "Your modules are loaded correctly. Proceeding to activate ASE..."
+fi
+
+echo "Changing directory to ~/software/python/virtualenvs/ase ..."
+cd ~/software/python/virtualenvs/ase
+
+function load_ase() {
+	source ~/software/python/virtualenvs/ase/bin/activate
+}
+
+if [[ $(pwd | grep 'ase') == */software/python/virtualenvs/ase ]]; then
+	pwd
+	echo "You are in the right location! Activating ase..."
+	load_ase
+else
+	echo "Please ensure you have the correct directory struture (~/software/python/virtualenvs/ase)..."
+	echo "Exiting"
+	exit 1
+fi
