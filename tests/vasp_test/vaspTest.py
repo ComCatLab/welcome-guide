ebd2e2a57a7d5fdecaa72b8a76aef289417b9424
Ugochukwu Nwosu <ugn@sfu.ca>
2024-11-06 00:28:13 -0800

    Refactor tests
    Signed-off-by: Ugochukwu Nwosu <ugn@sfu.ca>

--- tests/vasp_test/vaspTest.py
+++ tests/vasp_test/vaspTest.py
@@ -1,17 +1,20 @@
 from ase.build import molecule
-
-atoms = molecule("N2")
-atoms.center(vacuum=5)
-
-atoms.pbc = True  # VASP cannot handle non-periodic boundary conditions
 from ase.calculators.vasp import Vasp
+import pytest
 
-calc = Vasp(
-    xc="pbe",  # Select exchange-correlation functional
-    encut=400,  # Plane-wave cutoff
-    kpts=(1, 1, 1),  # k-points
-)
 
-atoms.calc = calc
-e = atoms.get_potential_energy()  # This call will start the calculation
-print("Potential energy: {:.2f} eV".format(e))
+@pytest.mark.calculator
+def test_vasp_calculator() -> None:
+    atoms = molecule("N2")
+    atoms.center(vacuum=5)
+
+    atoms.pbc = True  # VASP cannot handle non-periodic boundary conditions
+    calc = Vasp(
+        xc="pbe",  # Select exchange-correlation functional
+        encut=400,  # Plane-wave cutoff
+        kpts=(1, 1, 1),  # k-points
+    )
+
+    atoms.calc = calc
+    e = atoms.get_potential_energy()  # This call will start the calculation
+    print(f"Potential energy: {e:.2f} eV")
