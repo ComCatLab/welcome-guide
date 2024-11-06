1bb528d9244af5e015918ddb4918aedf711a180b
Ugochukwu Nwosu <ugn@sfu.ca>
2024-11-06 01:53:59 -0800

    Refactor tests; conditionally skip VASP tests
    This refactor ensures that calculation outputs are written to temporary
    directories to avoid cluttering the directory in which the tests are run.
    
    Repeat code is moved into pytest fixtures, and tests are refactored for
    simplicity.
    
    Tests are renamed more descriptively, and assertion statements are added
    to clarify test objectives.
    
    Signed-off-by: Ugochukwu Nwosu <ugn@sfu.ca>

--- tests/test_vasp.py
+++ tests/test_vasp.py
@@ -1,10 +1,24 @@
+import os
+from pathlib import Path
+
 from ase.build import molecule
 from ase.calculators.vasp import Vasp
 import pytest
 
+VASP_INSTALLED = (
+    os.environ.get("ASE_VASP_COMMAND")
+    or os.environ.get("VASP_COMMAND")
+    or os.environ.get("VASP_SCRIPT")
+)
+
 
 @pytest.mark.calculator
-def test_vasp_calculator() -> None:
+@pytest.mark.skipif(
+    not VASP_INSTALLED,
+    reason="The environment is not configured for VASP. "
+    "See the ASE documentation for details.",
+)
+def test_should_perform_vasp_calculation(tmp_path: Path) -> None:
     atoms = molecule("N2")
     atoms.center(vacuum=5)
 
@@ -13,8 +27,10 @@ def test_vasp_calculator() -> None:
         xc="pbe",  # Select exchange-correlation functional
         encut=400,  # Plane-wave cutoff
         kpts=(1, 1, 1),  # k-points
+        directory=tmp_path,
     )
 
     atoms.calc = calc
     e = atoms.get_potential_energy()  # This call will start the calculation
     print(f"Potential energy: {e:.2f} eV")
+    assert e is not None
