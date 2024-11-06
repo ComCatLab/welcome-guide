0a1ad55ca24db4f2a4da246f144ef83bbdb6e20f
Ugochukwu Nwosu <ugn@sfu.ca>
2024-11-06 01:53:59 -0800

    Refactor tests; conditionally skip VASP tests
    This refactor ensures that calculation outputs are written to temporary
    directories to avoid cluttering the directory in which the tests are run.
    
    Repeat code is moved into pytest fixtures, and tests are refactored for
    simplicity.
    
    Tests are renamed more descriptively, and assertion statements are added
    to clarify test objectives.
    
    Signed-off-by: Ugochukwu Nwosu <ugn@sfu.ca>

--- tests/test_gpaw.py
+++ tests/test_gpaw.py
@@ -1,8 +1,10 @@
+from collections.abc import Generator
+from pathlib import Path
+
 import matplotlib.pyplot as plt
 import pytest
 from ase import Atoms
-from ase.io import iread
-from ase.io.trajectory import Trajectory
+from ase.calculators.calculator import Calculator
 
 try:
     from gpaw import GPAW
@@ -10,74 +12,61 @@ except ImportError:
     GPAW = None
 
 
+@pytest.fixture(name="n2")
+def fixture_n2(gpaw_calc: Calculator) -> Atoms:
+    n2 = Atoms("N2", positions=[[0, 0, -1], [0, 0, 1]])
+    n2.center(vacuum=3.0)
+    n2.calc = gpaw_calc
+    return n2
+
+
+@pytest.fixture(name="gpaw_calc")
+def fixture_gpaw_calc(tmp_path: Path) -> Generator[Calculator, None, None]:
+    output_file = tmp_path.joinpath("gpaw.txt")
+    with output_file.open(mode="w", encoding="utf-8") as file:
+        yield GPAW(mode="lcao", basis="dzp", txt=file, xc="LDA")
+
+
 @pytest.mark.calculator
-@pytest.mark.skipif(GPAW is None, reason="GPAW is not installed")
-def test_gpaw_calculator() -> None:
-    atoms = Atoms("N2", positions=[[0, 0, -1], [0, 0, 1]])
-    atoms.write("myatoms.traj")
-    calc = GPAW(mode="lcao", basis="dzp", txt="gpaw.txt", xc="LDA")
+@pytest.mark.skipif(GPAW is None, reason="GPAW is not installed.")
+def test_should_calculate_n2_energy_and_forces(n2: Atoms) -> None:
+    e = n2.get_potential_energy()
+    f = n2.get_forces()
 
-    atoms.calc = calc
-
-    atoms.center(vacuum=3.0)
-    print(atoms)
-
-    e = atoms.get_potential_energy()
     print("The Energy is: ", e)
-
-    f = atoms.get_forces()
     print("Forces \n", f)
 
+    assert None not in (e, f)
+
 
 @pytest.mark.calculator
-@pytest.mark.skipif(GPAW is None, reason="GPAW is not installed")
-def test_should_perform_gpaw_calculation() -> None:
-    # Creates an N2 molecule with z positions of the N atoms
-    atoms = Atoms("N2", positions=[[0.0, 0.0, -1.0], [0.0, 0.0, 1.0]])
-
-    # Loads the N2 into the ase gui and displays them
-    # view(atoms)
-
-    atoms.write("myatoms.traj")
-
-    calc = GPAW(mode="lcao", basis="dzp", txt="gpaw.txt", xc="LDA")
-
-    atoms.calc = calc
-
-    atoms.center(vacuum=3.0)
-
-    print(atoms)
-
-    dstep = 0.05
+@pytest.mark.skipif(GPAW is None, reason="GPAW is not installed.")
+def test_should_calculate_energy_vs_n2_bond_length(n2: Atoms, tmp_path: Path) -> None:
+    # Distances are in picometers
+    start = 50
+    stop = 300
+    dstep = 5
     energies = []
     distances = []
 
-    ## Prints initial position and sets the position of the second N to be 0.05 A from the first N and relcaulate energy and forces
-    # print(atoms.positions)
-    # atoms.positions[1, 2] = atoms.positions[0, 2] + 0.05
-    # print(atoms.positions)
+    # Move the second N in lengths of dstep from 0.5 Å separation to 3 Å
+    # separation and updates energies and forces
+    for d in range(start, stop, dstep):
+        n2.positions[1, 2] = n2.positions[0, 2] + d
+        e = n2.get_potential_energy()
+        f = n2.get_forces()
 
-    traj = Trajectory("mytrajectory.traj", "w")
+        energies.append(e)
+        distances.append(d)
 
-    # Move the second N in lengths of dstep from 0.5 A separation to 5 A separation and updates energies and forces
-    for i in range(int((3.0 - 0.5) / dstep)):
-        d = 0.5 + i * dstep
-        atoms.positions[1, 2] = atoms.positions[0, 2] + d
-        print(atoms.positions)
-        e = atoms.get_potential_energy()
+        print(n2.positions)
         print("The Energy is: ", e, " eV")
-
-        f = atoms.get_forces()
         print("The Forces on each atom are: ", f, " ev/A")
-        traj.write(atoms)
-
-    # This extracts the energies and distances from the trajectory file and appends them to the appropriate arrays
-    for atoms in iread("mytrajectory.traj"):
-        energies.append(atoms.get_potential_energy())
-        distances.append(atoms.positions[1, 2] - atoms.positions[0, 2])
 
     ax = plt.gca()
     ax.plot(distances, energies)
     ax.set_xlabel("Distance [A]")
     ax.set_ylabel("Total Energy [eV]")
-    plt.savefig("N2_distances_vs_energy.png", bbox_inches="tight")
+    figure = tmp_path.joinpath("N2_distances_vs_energy.png")
+    plt.savefig(figure, bbox_inches="tight")
+    assert figure.exists()
