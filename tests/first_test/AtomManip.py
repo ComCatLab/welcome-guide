57e4746853893449744d3b4d4d7caa00b6df98c3
Ugochukwu Nwosu <ugn@sfu.ca>
2024-11-05 21:19:37 -0800

    Move tests to test dir and format with ruff
    Signed-off-by: Ugochukwu Nwosu <ugn@sfu.ca>

---
+++ tests/first_test/AtomManip.py
@@ -0,0 +1,44 @@
+# Standard imports for ase
+from math import sqrt
+import numpy as np
+from ase import Atoms
+from ase import visualize
+from ase.visualize import view
+from ase.io import read
+from ase.build import fcc111
+
+a = 3.55
+atoms = Atoms(
+    "Ni4",
+    cell=[sqrt(2) * a, sqrt(2) * a, 1.0, 90, 90, 120],
+    pbc=(1, 1, 0),
+    scaled_positions=[(0, 0, 0), (0.5, 0, 0), (0, 0.5, 0), (0.5, 0.5, 0)],
+)
+atoms.center(vacuum=5.0, axis=2)
+
+atoms.write("slab.xyz")
+
+h = 1.9
+relative = (1 / 6, 1 / 6, 0.5)
+absolute = np.dot(relative, atoms.cell) + (0, 0, h)
+atoms.append("Ag")
+atoms.positions[-1] = absolute
+
+# view(atoms)
+W = read("WL.traj")
+
+slab = fcc111("Ni", size=[2, 4, 3], a=3.55, orthogonal=True)
+
+W.cell = [W.cell[1, 1], W.cell[0, 0], 0.0]
+W.rotate(90, "z", center=(0, 0, 0))
+W.wrap()
+W.set_cell(slab.cell, scale_atoms=True)
+
+zmin = W.positions[:, 2].min()
+zmax = slab.positions[:, 2].max()
+
+W.positions += (0, 0, zmax - zmin + 1.5)
+
+interface = slab + W
+interface.center(vacuum=6, axis=2)
+interface.write("NiH2O.traj")
--- tests/gpaw_test/example.py
+++ tests/gpaw_test/example.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 
 from ase import Atoms
-atoms = Atoms('N2', positions=[[0, 0, -1], [0, 0, 1]])
+
+atoms = Atoms("N2", positions=[[0, 0, -1], [0, 0, 1]])
 
 # from ase.visualize import view
 # view(atoms)
 
 from ase.io import write
-write('myatoms.traj', atoms)
+
+write("myatoms.traj", atoms)
 
 from gpaw import GPAW
 
-calc = GPAW(mode='lcao', basis='dzp', txt='gpaw.txt', xc='LDA')
+calc = GPAW(mode="lcao", basis="dzp", txt="gpaw.txt", xc="LDA")
 
 atoms.calc = calc
 
@@ -19,7 +21,7 @@ atoms.center(vacuum=3.0)
 print(atoms)
 
 e = atoms.get_potential_energy()
-print('The Energy is: ', e)
+print("The Energy is: ", e)
 
 f = atoms.get_forces()
-print('Forces \n', f)
+print("Forces \n", f)
