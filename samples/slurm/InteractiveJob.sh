8d4a1ee54974e283ec3b26a0671157e447ec5d08
Ugochukwu Nwosu <ugognw@gmail.com>
2024-11-10 06:24:44 -0800

    Delete duplicate code
    Signed-off-by: Ugochukwu Nwosu <ugognw@gmail.com>

--- samples/slurm/InteractiveJob.sh
+++ samples/slurm/InteractiveJob.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # This script simply initiates an interactive session on the cluster to avoid using the login node.
 # The session has 4 cores, 12 GB memory, X11 forwarding, and a 1 hour time limit.
-(cd ~/scratch/;salloc --x11 --time=01:00:00 --mem-per-cpu=3G --ntasks=4 --account=$SALLOC_ACCOUNT)
-
-#!/bin/bash
-# This script simply initiates an interactive session on the cluster to avoid using the login node.
-# The session has 4 cores, 12 GB memory, X11 forwarding, and a 1 hour time limit.
-(cd ~/scratch/;salloc --x11 --time=01:00:00 --mem-per-cpu=3G --ntasks=4 --account=$SALLOC_ACCOUNT)
+(cd ~/scratch/ || exit; salloc --x11 --time=01:00:00 --mem-per-cpu=3G --ntasks=4 --account="$SALLOC_ACCOUNT")
--- samples/slurm/activateASEORCA.sh
+++ samples/slurm/activateASEORCA.sh
@@ -14,39 +14,7 @@ else
 fi
 
 echo "Changing directory to ~/software/python/virtualenvs/ase ..."
-cd ~/software/python/virtualenvs/ase
-
-function load_ase() {
-	source ~/software/python/virtualenvs/ase/bin/activate
-}
-
-if [[ $(pwd | grep 'ase') == */software/python/virtualenvs/ase ]]; then
-	pwd
-	echo "You are in the right location! Activating ase..."
-	load_ase
-else
-	echo "Please ensure you have the correct directory struture (~/software/python/virtualenvs/ase)..."
-	echo "Exiting"
-	exit 1
-fi
-
-#!/bin/bash
-
-module purge
-module load StdEnv/2020 gcc/10.3.0 openmpi/4.1.1
-module load orca/5.0.4
-module load python/3.11.5 scipy-stack
-
-if [[ $(module list | grep 'intel/2023.2.1') == ""  || $(module list | grep 'python/3.11.5') == "" || $(module list | grep 'vasp/6.4.2') == "" ]]; then
-	echo "Your modules are not loaded correctly for ORCA. Cancelling job... "
-	exit 1
-else
-	echo "Your modules are loaded correctly for ORCA. Proceeding to activate ASE..."
-	export PATH="${EBROOTORCA}/:$PATH"
-fi
-
-echo "Changing directory to ~/software/python/virtualenvs/ase ..."
-cd ~/software/python/virtualenvs/ase
+cd ~/software/python/virtualenvs/ase || exit
 
 function load_ase() {
 	source ~/software/python/virtualenvs/ase/bin/activate
