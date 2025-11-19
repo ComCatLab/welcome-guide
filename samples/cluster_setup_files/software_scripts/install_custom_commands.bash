ce4c322a7b5c8b1d235a9c9979f5e209b3762236
Ugochukwu Nwosu <ugognw@gmail.com>
2025-11-18 20:08:30 -0800

    Fix software scripts; add where-to-edit comments
    Fix sed command: the MacOS implementation of sed (BSD sed) requires an argument
    for the -i option denoting the extension for backup files, but the version
    installed on DRA clusters (GNU sed) does not. This causes sed to interpret
    the sed script as a file on DRA clusters and causes an error.
    
    The new comments direct users to where they should edit the software scripts
    in the case that they download files individually.
    
    Commands that append lines to files are edited to properly add newlines.
    
    Signed-off-by: Ugochukwu Nwosu <ugognw@gmail.com>

--- samples/cluster_setup_files/software_scripts/install_custom_commands.bash
+++ samples/cluster_setup_files/software_scripts/install_custom_commands.bash
@@ -5,8 +5,9 @@ software_home=$1
 if [[ $software_home = "" ]]; then echo "Error: No software home directory specified"; exit; fi
 if ! test -e "$software_home"; then echo "Error: Software home directory $software_home does not exist"; exit; fi
 
+# Change this relative path to point to the custom commands archive
 cp -v sources/custom_commands.tar.zst "$software_home"
 cd "$software_home" || exit
 tar -vxf "custom_commands.tar.zst" && rm -v "custom_commands.tar.zst"
 cd ~- || exit
-echo "\nmodule load custom-commands" >> ~/.bashrc
+{ echo ""; echo "module load custom-commands"; } >> ~/.bashrc
